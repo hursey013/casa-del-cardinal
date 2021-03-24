dcfbe0b43672a004b4392440b2eb7f668b8fe7cc
Brian Hurst <hursey013@pm.me>
2021-03-23 20:12:28 -0400

    Add Twitter integration

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const fileMiddleware = require("express-multipart-file-parser");
 const functions = require("firebase-functions");
 const express = require("express");
 const tfnode = require("@tensorflow/tfjs-node");
+const Twit = require("twit");
+
+require("dotenv").config();
 
 const labels = require("./labels.json");
 
@@ -11,6 +14,13 @@ const threshold = 33;
 const app = express();
 app.use(fileMiddleware);
 
+const T = new Twit({
+  consumer_key: functions.config().twitter.consumer_key,
+  consumer_secret: functions.config().twitter.consumer_secret,
+  access_token: functions.config().twitter.access_token,
+  access_token_secret: functions.config().twitter.access_token_secret
+});
+
 const decodeImage = buffer =>
   resizeImage(
     tfnode.node
@@ -19,6 +29,9 @@ const decodeImage = buffer =>
       .div(255)
       .expandDims(0)
   );
+
+const imageToBase64 = stream =>
+  Buffer.from(stream, "binary").toString("base64");
 
 const resizeImage = image =>
   tfnode.image.resizeBilinear(image, (size = [imageSize, imageSize]));
@@ -33,6 +46,25 @@ const getPredictions = async buffer => {
   return await model.predict(await decodeImage(buffer)).dataSync();
 };
 
+const postTweet = (media_data, results) =>
+  T.post("media/upload", { media_data })
+    .then(({ data }) =>
+      T.post("media/metadata/create", {
+        media_id: data.media_id_string,
+        alt_text: {
+          text: results.common_name
+        }
+      }).then(res =>
+        T.post("statuses/update", {
+          status: `Spotted a ${results.common_name.toLowerCase()} at the bird feeder!`,
+          media_ids: [data.media_id_string]
+        })
+      )
+    )
+    .catch(error => {
+      console.error("Media upload failed");
+    });
+
 app.post("/", async (req, res) => {
   const { buffer } = req.files[0];
   const predictions = await getPredictions(buffer);
@@ -42,8 +74,10 @@ app.post("/", async (req, res) => {
   const results = { ...label, score };
 
   if (score >= threshold && label.id !== 964) {
-    functions.logger.info(results);
+    postTweet(imageToBase64(buffer), results);
   }
+
+  functions.logger.info(results);
 
   return res.status(200).send(results);
 });
